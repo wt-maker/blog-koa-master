63d3d4d313ffc77acf3f25cd4dc62fdd065ba4c6
wt <wangtong0216@outlook.com>
2020-06-27 09:33:42 +0800

    后台修改

--- src/routers/tag.js
+++ src/routers/tag.js
@@ -78,7 +78,7 @@ const tagRouter = (router) => {
             errorResponse({ ctx, status: RESPONSE_CODE.RESPONSE_CODE_NOT_FOUND, message: '查询失败', error: '缺少参数id' })
         }
     }
-    router.get('/getTag/:id', GET_ARTICLE_BY_ID)
+    router.get('/getTag/:id', GET_TAG_BY_ID)
 }
 
 module.exports = tagRouter
