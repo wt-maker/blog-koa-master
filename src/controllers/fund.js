88b1020de81743f4edff96974418da1fd5c01a85
wt <wangtong0216@outlook.com>
2020-06-30 20:05:14 +0800

    fund 查询

--- src/controllers/fund.js
+++ src/controllers/fund.js
@@ -2,12 +2,63 @@ const Fund = require('../models/fund')
 const charset = require('superagent-charset')
 const superagent = charset(require('superagent'))
 const jsdom = require('jsdom')
-const {JSDOM} = jsdom
+const { JSDOM } = jsdom
 
-const getCurrentData = async (ctx) => {
-    
+const getFundDataById = async (id) => {
+    let url = `http://fund.eastmoney.com/${id}.html?spm=search`
+    return new Promise((resolve, reject) => {
+        superagent
+            .get(url)
+            .end((err, sres) => {
+                if (err) {
+                    reject(err)
+                }
+                resolve(sres.text)
+            })
+    })
+}
+
+
+const getFundData = async (arr) => {
+    let res = []
+    return new Promise((resolve, reject) => {
+        let length = arr.length
+        arr.map( async (item, index) => {
+            let data = await getFundDataById(item.serialNumber)
+            let dom = new JSDOM(data)
+            let applies = dom.window.document.getElementById('gz_gszzl').innerHTML
+            res.push({
+                applies,
+                serialNumber: item.serialNumber,
+                name: item.name,
+                position: item.position
+            })
+            if (length === index+1) {
+                resolve(res)
+            }
+        })
+    })
+}
+
+const addFund = async (data) => {
+    let res = null
+    if(data) {
+        res = new Fund(data).save()
+    }
+    return res
+}
+
+const getAllFund = async() => {
+    let res = null
+    await Fund.find({}, (err, doc) => {
+        res = doc
+    })
+    return res
 }
 
 module.exports = {
-    getCurrentData
+    getFundDataById,
+    addFund,
+    getAllFund,
+    getFundData
 }
--- src/routers/fund.js
+++ src/routers/fund.js
@@ -1,31 +1,51 @@
-const { getCurrentData } = require('../controllers/fund')
+const { getFundDataById, addFund, getAllFund, getFundData } = require('../controllers/fund')
 const { successResponse, errorResponse } = require('../utils/responstHandle')
-const charset = require('superagent-charset')
-const superagent = charset(require('superagent'))
+const { RESPONSE_CODE } = require('../config')
 const jsdom = require('jsdom')
 const { JSDOM } = jsdom
 
 const fundRouter = (router) => {
-    const cheerio = require('cheerio')
-    const charset = require('superagent-charset')
-    const superagent = charset(require('superagent'))
-    
-    let res = {}
 
-    const GET_FUND = ((ctx, next) => {
-        let {id} = ctx.params
-        let url = `http://fund.eastmoney.com/${id}.html?spm=search`
-        superagent.get(url)
-            .charset('gbk')
-            .buffer(true)
-            .end((err, data) => {
-                let dom = new JSDOM(data.text)
-                let applies = dom.window.document.getElementById('gz_gszzl').innerHTML
-                res = {applies}
-            })
-        ctx.body = res
-    })
-    router.get('/getFund/:id',  GET_FUND)  
+    const ADD_FUND = async (ctx, next) => {
+        let data = ctx.request.body
+        try {
+            let res = await addFund(data)
+            successResponse({ ctx, message: '插入成功', res })
+        } catch (error) {
+            errorResponse({ ctx, status: RESPONSE_CODE.RESPONSE_CODE_SERVER_ERROR, message: '插入失败', error })
+        }
+    }
+
+    router.post('/addFund', ADD_FUND)
+
+    const GET_FUND_BY_ID = async (ctx, next) => {
+        let { id } = ctx.params
+        try {
+            let data = await getFundDataById(id)
+            let dom = new JSDOM(data)
+            let applies = dom.window.document.getElementById('gz_gszzl').innerHTML
+            let res = { applies }
+            successResponse({ ctx, message: '查询成功', res })
+        } catch (error) {
+            errorResponse({ ctx, status: RESPONSE_CODE.RESPONSE_CODE_SERVER_ERROR, message: '查询失败', error })
+        }
+    }
+    router.get('/getFund/:id', GET_FUND_BY_ID)
+
+    const GET_ALL_FUND = async (ctx, next) => {
+        try {
+            let fundList = await getAllFund()
+
+            let data = await getFundData(fundList)
+            
+            let res = {fundList : data}
+
+            successResponse({ ctx, message: '查询成功', res })
+        } catch (error) {
+            errorResponse({ ctx, status: RESPONSE_CODE.RESPONSE_CODE_SERVER_ERROR, message: '查询失败', error })
+        }
+    }
+    router.get('/getAllFund', GET_ALL_FUND)
 }
 
 module.exports = fundRouter
